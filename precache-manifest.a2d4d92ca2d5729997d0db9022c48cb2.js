6d3f0bab9ecc82416887dc41c06154600ceff164
Yovel Becker <yovelb13@gmail.com>
2019-12-12 15:44:48 +0200

    deploy

---
+++ precache-manifest.a2d4d92ca2d5729997d0db9022c48cb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-media-recorder/robots.txt"
+  },
+  {
+    "revision": "d56e27939a6e29e35c9a",
+    "url": "/vue-media-recorder/js/chunk-vendors.0b375f97.js"
+  },
+  {
+    "revision": "50d70a79bcce067f9f63",
+    "url": "/vue-media-recorder/js/app.19c79260.js"
+  },
+  {
+    "revision": "5b931009fea9d3758702eaeafa0da0cf",
+    "url": "/vue-media-recorder/index.html"
+  },
+  {
+    "revision": "50d70a79bcce067f9f63",
+    "url": "/vue-media-recorder/css/app.73525a57.css"
+  }
+];
